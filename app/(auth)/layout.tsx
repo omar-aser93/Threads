1a9eb1b4a012f19ee62da06ec63c470f4a4735e7
omar-mohamed <omaraser93@yahoo.com>
2024-09-03 18:28:16 +0300

    implement 2  commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";        //ClerkProvider we wrap it
 import { dark } from "@clerk/themes";                 //Dark theme for clerk forms, we pass it to ClerkProvider
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +16,8 @@ export default function RootLayout({ children}: Readonly<{ children: React.React
   return (
     <ClerkProvider appearance={{ baseTheme: dark }} >
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>
-          <div className="w-full flex justify-center items-center min-h-screen"> {children} </div> 
+        <body className={`${inter.className} bg-dark-1 `}>
+          <div className="w-full flex justify-center items-center min-h-screen" > {children} </div> 
         </body>
       </html>
     </ClerkProvider>
